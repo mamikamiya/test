08c10fc9b5204cf2b1c84cd8b5d361d4180cf466
mami.kamiya <mami.kamiya@mixi.co.jp>
2018-01-04 10:57:26 +0900

    取り急ぎコミット

---
+++ 0101/hoge.js
@@ -0,0 +1,38 @@
+var num = 125;
+if (num % 2 == 0 ){
+	window.alert(num + 'は偶数です');
+}else{
+	window.alert(num + 'は奇数です');
+}
+
+
+// var a, b, c;
+// a=0 || 10;
+// b=5 || 10;
+// c='' || 'Hello';
+
+// window.alert(a);
+// window.alert(b);
+// window.alert(c);
+
+
+// var a = 10, b = 20;
+// var c = a > 20 || b < 30;
+
+// window.alert(c);
+
+// var a,b,c;
+
+// a=0 && 10;
+// b=5 && 10;
+// c=5 && 'Hello';
+
+// window.alert(a);
+// window.alert(b);
+// window.alert(c);
+
+
+
+// var bool = num > 5;
+// window.alert(bool == 1);
+// window.alert(bool == 0);
